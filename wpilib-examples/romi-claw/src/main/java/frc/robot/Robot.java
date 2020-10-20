00a554ec141b879079e4d681e1e380eefcf21a09
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2020-10-19 20:58:45 -0400

    WIP: java update

--- wpilib-examples/romi-claw/src/main/java/frc/robot/Robot.java
+++ wpilib-examples/romi-claw/src/main/java/frc/robot/Robot.java
@@ -83,6 +83,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    System.out.println("Entering teleop mode");
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
